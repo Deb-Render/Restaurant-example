5750998c2bacc3b41293a8d0a25f66ee3a75e0da
Deb-Render <deb606060@gmail.com>
2023-11-25 21:43:49 +1100

    mostly responsive needs a few small tewaks

--- js/main.js
+++ js/main.js
@@ -6,4 +6,32 @@ navToggle.addEventListener('click', function(){
     links.classList.toggle("show_nav");
 })
 
+// const mediaquery = matchmedia('(min-width: 675px)')
 
+
+// document.querySelector('#andiNext').addEventListener('click', andiNext)
+
+
+   function toggleImageDisplay() {
+   
+      var imageSlider = document.getElementById('image-slider');
+      var singleImage = document.getElementById('single-image');
+
+      if (window.innerWidth <= 675) {
+        
+         imageSlider.style.display = 'none';
+         singleImage.style.display = 'block';
+      } else {
+        
+         imageSlider.style.display = 'block';
+         singleImage.style.display = 'none';
+      }
+   }
+
+   
+   toggleImageDisplay();
+
+   
+   window.addEventListener('resize', toggleImageDisplay);
+
+//   Im sure there is an easier way to write out the above javascript. I will change it later when I learn more. For now this works.
